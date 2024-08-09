836523bccb1ced4f511dc57435f00e00f4fb662b
Aldiery Gonzalez <aldieryrgon@gmail.com>
2024-08-09 11:33:24 -0400

    Feat: Made loading spinner work without layout

--- app/components/loading/logo.tsx
+++ app/components/loading/logo.tsx
@@ -2,8 +2,8 @@ import SvgComponent from "../icons/logo";
 
 export function LoadingBillixer() {
     return (
-        <div className="my-auto flex items-center justify-center">
-            <SvgComponent className="my-auto h-36 w-36 animate-bounce" />
+        <div className="fixed inset-0 flex items-center justify-center">
+            <SvgComponent className="h-36 w-36 animate-bounce" />
         </div>
     );
 }
